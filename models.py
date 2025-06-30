ddc5d3ecab267674008d2126df003a3f384eb7f0
Black-tag <unnianandunni007@gmail.com>
2025-06-30 13:25:08 +0000

    OAuth2 with Password (and hashing), Bearer with JWT tokens

---
+++ models.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+
+
+
+class BugInput(BaseModel):
+    title: str
+    description: str 
+
+
+class BugRead(BugInput):
+    id: int
+    class Config:
+        orm_mode = True 
+
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
--- sql.py
+++ sql.py
@@ -16,6 +16,21 @@ class Bug(Base):
     title = Column(String)
     description = Column(String)
 
-
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "fakehashedsecret",
+        "disabled": False,
+    },
+    "alice": {
+        "username": "alice",
+        "full_name": "Alice Wonderson",
+        "email": "alice@example.com",
+        "hashed_password": "fakehashedsecret2",
+        "disabled": True,
+    },
+}
 
 Base.metadata.create_all(engine)
